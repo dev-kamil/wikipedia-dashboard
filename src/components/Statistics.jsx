8553b57b78d7f94c6cfeb258ce67a230c78718ca
dev-kamil <sackamacacacasaka@gmail.com>
2023-10-03 01:08:38 +0200

    Added placeholder while loading statistics

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,5 +1,18 @@
-const Statistics = ({statistics}) => {
-  if (!statistics) return <span>Loading...</span>;
+const Statistics = ({ statistics }) => {
+  if (!statistics)
+    return (
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+        {[...Array(4)].map((value, index) => (
+          <div
+            key={`stats-${index}`}
+            className="bg-white rounded-lg p-4 drop-shadow-2xl hover:bg-slate-50 dark:bg-slate-800 dark:hover:bg-slate-700 animate-pulse"
+          >
+            <p className="text-slate-500 dark:text-slate-400">Loading...</p>
+            <p className="font-bold text-2xl dark:text-slate-50">--</p>
+          </div>
+        ))}
+      </div>
+    );
 
   const articles = statistics.statistics.articles.toLocaleString();
   const users = statistics.statistics.users.toLocaleString();
